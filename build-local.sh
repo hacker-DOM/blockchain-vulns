3a3e17b7e602df2a91ef188843b147a06f38ca75
hacker-DOM <dteiml@gmail.com>
2022-04-15 17:25:26 +0200

    :sparkles: Add main.adoc

---
+++ build-local.sh
@@ -0,0 +1,2 @@
+./build.sh && \
+/usr/bin/osascript -e 'tell application "Alfred 4" to run trigger "refresh-compare" in workflow "com.alfredapp.dteiml.build-report"'
---
+++ build.sh
@@ -0,0 +1,3 @@
+IN_FILE="main.adoc";
+OUT_FILE="index.html";
+asciidoctor -o "$OUT_FILE" "$IN_FILE";
